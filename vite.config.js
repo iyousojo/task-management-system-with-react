b2ca35eaff20b661b30deb6d8a6fc568a17db3fb
solomon <solomonjohnbll676@gmail.com>
2025-06-20 17:09:16 +0100

    deploying the app

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/task-management-system-with-react/',  // important!
-})
+  base: '/task-management-system-with-react/',
+  plugins: [react()]
+});
+
