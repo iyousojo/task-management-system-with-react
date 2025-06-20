ea2205e2393af5774d476803463f3e0da92aa836
solomon <solomonjohnbll676@gmail.com>
2025-06-20 17:15:19 +0100

    still try to make it live

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/task-management-system-with-react/',
-  plugins: [react()]
-});
+  plugins: [react()],
+  base: '/', // <-- root
+})
+
 
